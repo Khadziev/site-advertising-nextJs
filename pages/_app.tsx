aaa70b40b1671d92a352c2c4fae6ec2af8402f61
Khadziev <82869463+Khadziev@users.noreply.github.com>
2022-08-20 00:50:42 +0300

    компоненты Htag, Button. настройка webpack, настройка next-env

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import { AppProps } from "next/dist/shared/lib/router/router";
 import "../styles/globals.css";
+import React from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Haz Khadziev</title>
+        <link rel="stylesheet" href="/favison.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
